1a164ea918b2f9cd410be348ceadcddaf1d57ef0
gargeedeshmukh <gargeedeshmukh@users.noreply.github.com>
2020-03-22 23:09:00 +0530

    updated with basic math and print messages

---
+++ Basic/Math.py
@@ -0,0 +1,12 @@
+#Testing various math operations
+print(6+4)
+print(6+4*2)
+type(16)
+print (2 ** 3)
+print ('''Akira is bored''')
+message="Akira is sleepy"
+a=23
+b=24
+c=a-b
+print(message)
+print(a,b,c)
